9f05716ffc73f7ef56149f3db8c683ee8b0b028f
胡赞來 <361206768@qq.com>
2021-05-17 15:44:02 +0800

    挪动

---
+++ JavaScript方法/instanceof.js
@@ -0,0 +1,4 @@
+// 是否在某个原型链上
+let a = '123';
+console.log(a instanceof String);
+
--- JavaScript方法/slice().js
+++ JavaScript方法/slice().js
@@ -1,18 +1,9 @@
 // slice()截取字符串的一部分。不对目标字符串做永久处理。
 
-// stringObject.slice(start,end)，如果未指定end，则截取到末尾。
-
-
 let str = "abcde";
 
-console.log(str.slice(0,2));
-// ab
-
-console.log(str.slice(1,1));
-// 什么都没
-
-console.log(str.slice(1,2));
-// b
-
-console.log(str.slice(2));
-// cde
+// stringObject.slice(start,end)
+console.log(str.slice(0,2)); // ab
+console.log(str.slice(1,1)); // 什么都没
+// 如果未指定end，则截取到末尾
+console.log(str.slice(2)); // cde
