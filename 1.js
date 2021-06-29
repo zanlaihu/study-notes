27016227147e24bd5a49fdae190aa2bfcc21d7b5
胡赞來 <361206768@qq.com>
2021-06-29 17:45:41 +0800

    m

--- 1.js
+++ 1.js
@@ -1,5 +1,7 @@
-const url = "www.baidu.com?123";
+const url = "www.baidu.com?param1=123&param2=231";
 
-const url = "www.baidu.com";
+const url2 = "www.baidu.com";
 
-const param = (url.)
+const params = (url.split('?')[1].length > 0 ? url.split('?')[1] : '');
+
+console.log(params);
