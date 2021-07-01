031a0370726cbfcc81bbd050b53d482674644bef
胡赞來 <361206768@qq.com>
2021-07-01 23:02:09 +0800

    m

---
+++ JavaScript/原生方法/Math.min().js
@@ -0,0 +1,13 @@
+/** 返回一组数中的最小值
+ *
+ * @requires Array
+ * @return number
+ */
+
+ Math.min(1, 2, 3);
+ // return: 1
+ 
+ const array = [1, 2, 3, -10];
+ console.log(Math.min(...array));
+ // output: -10
+ 
---
+++ 算法/中心扩散法/最长回文子串.js
@@ -0,0 +1,6 @@
+function longestPalindrome(str){
+    for (let i = 0; i<str.length;i++){
+        originStr = str[i];
+        for (let j = i + 1;j<str.length)
+    }
+}
