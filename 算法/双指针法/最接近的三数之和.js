78278fd1101d61674190d27c11f68a9104730435
胡赞來 <361206768@qq.com>
2021-07-13 15:02:29 +0800

    m

---
+++ 算法/双指针法/最接近的三数之和.js
@@ -0,0 +1,14 @@
+function threeSumCloest(nums, target){
+    let cloestNum = 0;
+
+    for (let i = 0; i < nums.length -2; i++){
+        left = i + 1;
+        right = nums.length - 1;
+
+        while (left < right){
+            trySum = nums[i] + nums[left] + nums[right];
+            diff = target - diff;
+            if ()
+        }
+    }
+}
