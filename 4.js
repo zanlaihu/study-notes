0af0bdfc6a36e4aa95e6d8b49d712f8717157ec3
胡赞來 <361206768@qq.com>
2021-08-03 00:29:27 +0800

    add

--- 4.js
+++ 4.js
@@ -1,15 +1,28 @@
-function h(x) {
-  return f(g(x));
+function f1(arg){
+  console.log("f1",arg)
+  return arg;
 }
-
-function f(x) {
-  return x + 1;
+function f2(arg){
+  console.log("f2",arg)
+  return arg;
 }
-
-function g(x) {
-  return 2 * x;
+function f3(arg){
+  console.log("f3",arg)
+  return arg;
 }
-
-const result = h(3);
-
-console.log(result);
+function compose(...funcs){
+  if(funcs.length===0){
+      return arg=>arg;
+  }
+  if(funcs.length===1){
+      return funcs[0];
+  }
+  // return funcs.reduce((a,b)=>(...args)=>a(b(...args)))
+  return funcs.reduce(function(a,b){
+      return function(o){
+          return a(b(o))
+      }
+  })
+}
+let res = compose(f1,f2,f3)("omg");//f1(f2(f3("omg"))) 洋葱模型
+console.log("res",res);
