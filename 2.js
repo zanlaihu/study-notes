8a8e4fc171a31b4ebe5b696425587c44cb45840a
胡赞來 <361206768@qq.com>
2021-09-10 18:30:31 +0800

    m

--- 2.js
+++ 2.js
@@ -1,2 +1,19 @@
-let p = new Promise(() => {});
-setTimeout(console.log, 0, p); // Promise <pending>
+function getFi(count){
+    let first = 1;
+    let second = 1;
+
+    if(count === 1 || count === 2){
+        return 1;
+    }
+
+    const times = count - 2;
+
+    for (i = 0; i < times + 1; i++){
+        newValue = first + second;
+        first = second;
+        second = newValue;
+    }
+    return newValue;
+}
+
+console.log(getFi(6));
