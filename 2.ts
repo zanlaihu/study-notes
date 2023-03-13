ba0f99638084bdb6161bb916eaf1b0194068919c
huzanlai <huzanlai.hzl@alibaba-inc.com>
2023-03-13 11:08:57 +0800

    chore: 调整

--- 2.ts
+++
@@ -1,7 +0,0 @@
-
-
-private scrollToTop():void{
-  if (this.getContent("ion-content")) {
-  this.getContent("ion-content").scrollToTop();
-}
-}
--- 6.js
+++ 6.js
@@ -1,14 +1,10 @@
-(function() {
+setTimeout(function cb() {
+  console.log("这是来自第一个回调的消息");
+}, 3);
 
-  setTimeout(function cb() {
-    console.log('这是来自第一个回调的消息');
-  }, 3);
-
-  setTimeout(function cb1() {
-    console.log('这是来自第二个回调的消息');
-  }, 0);
-
-})();
+setTimeout(function cb1() {
+  console.log("这是来自第二个回调的消息");
+}, 0);
 
 // "这是来自第一个回调的消息"
 // "这是来自第二个回调的消息"
